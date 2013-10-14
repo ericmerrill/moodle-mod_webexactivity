2d61f8aba50ac3f5e2035597c676aad7085e9d67
Eric Merrill <merrill@oakland.edu>
2013-10-14 08:16:17 -0400

    Adding headers and such to files

--- lib.php
+++ lib.php
@@ -22,6 +22,8 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die();
+
 function webexactvity_add_instance($data);
 
 function webexactvity_update_instance($data);
