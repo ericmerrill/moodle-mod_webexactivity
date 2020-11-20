1f3fce189ae932d7fddbd3870612e2d8e3fc7730
Eric Merrill <merrill@oakland.edu>
2020-11-20 09:52:12 -0500

    Add start of email script

---
+++ classes/recording_notifier.php
@@ -0,0 +1,107 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * A class for sending out notifications for transfered recordings.
+ *
+ * @package    mod_webexactvity
+ * @author     Eric Merrill <merrill@oakland.edu>
+ * @copyright  2020 Oakland University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_webexactivity;
+
+require_once($CFG->libdir . '/filelib.php');
+
+// use \mod_webexactivity\local\type;
+use mod_webexactivity\recording;
+use curl;
+use stdClass;
+use core\lock\lock_config;
+use mod_webexactivity\local\exception;
+
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * A class for sending out notifications for transfered recordings.
+ *
+ * @package    mod_webexactvity
+ * @author     Eric Merrill <merrill@oakland.edu>
+ * @copyright  2020 Oakland University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class recording_notifier {
+
+    /** @var recording The recording object we are downloading. */
+    private $recording = null;
+
+    /**
+     * Builds the recording object.
+     *
+     * @param recording    $recording Object of recording
+     * @throws coding_exception when bad parameter received.
+     */
+    public function __construct(recording $recording) {
+        $this->recording = $recording;
+    }
+
+    public function get_email_addresses() {
+        global $DB;
+
+        $hostid = $this->recording->hostid;
+
+        // See if we already know the user.
+        $webexuser = user::load_webex_id($hostid);
+        if (!empty($webexuser)) {
+            if (!empty($webexuser->moodleuserid)) {
+                // Use the Moodle email address if it exists.
+                if ($user = $DB->get_record('user', ['id' => $webexuser->moodleuserid])) {
+                    return [$user->email];
+                }
+            }
+
+            // Now use the Webex user email address if we have it.
+            if (!empty($webexuser->email)) {
+                return [$webexuser->email];
+            }
+        }
+
+        // We don't have the user's object or email address. Now we ask Webex directly.
+        $user = user::search_webex_for_webexid($hostid);
+        if (!empty($user) && !empty($user->email)) {
+            return [$user->email];
+        }
+
+        return false;
+    }
+
+    public function create_message() {
+
+    }
+
+
+    public function log($msg) {
+        if (defined('CLI_SCRIPT')) {
+            mtrace('Recording ' . $this->recording->id . ': ' . $msg);
+        }
+    }
+
+
+
+
+}
